2dfeeea32aa83233ef4bc5a4c40f73e397ab2f7a
anderllr <anderllr@gmail.com>
2018-10-11 11:38:12 -0400

    Setup Nginx

--- server/index.js
+++ server/index.js
@@ -50,7 +50,7 @@ if (process.env.NODE_ENV === "production") {
 	app.use(express.static("../client/build"));
 
 	// Return the main index.html, so react-router render the route in the client
-	app.get("/", (req, res) => {
+	app.get(["/flamingo", "/flamingo/*"], (req, res) => {
 		res.sendFile(path.resolve("../client/build", "index.html"));
 	});
 }
